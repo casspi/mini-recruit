4b2520299f99564377ea7d61e265391c9591e7d8
kassadin <369642339@qq.com>
2023-07-05 09:40:52 +0800

    feat: subcode

--- src/views/pages/home/filter/index.js
+++ src/views/pages/home/filter/index.js
@@ -20,6 +20,8 @@ new WowPage({
     let {api$, params$} = this.data
     // 疾病字典
     this.curl(api$.DIC_FILTER_DISEASE, {}, {method: 'get'}).then((res = []) => {
+      const omitDisease = ["全部肿瘤", "其他肿瘤", "全部慢病", "其他慢病"]
+      res = res.filter(o => !omitDisease.includes(o.label))
       let options = res.map(item => {
         // 有些疾病没有children 这里补个默认的
         if (!item.children || !item.children.length) {
